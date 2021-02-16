452244386ae56c707dfea6da9114532b301d2541
Jenise Ruehle <jeniseruehle@gmail.com>
2021-02-16 11:56:18 -0600

    adding errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
+            flash[:error] = "Uh oh! Please fill out all fields to create account."
             render :new
         end
     end
