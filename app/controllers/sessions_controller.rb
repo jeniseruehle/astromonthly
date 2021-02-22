e2d1eb230ad0e97e80d4b49bef070c7ffd3a2604
Jenise Ruehle <jeniseruehle@gmail.com>
2021-02-22 12:12:58 -0600

    OAuth update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,27 +3,38 @@ class SessionsController < ApplicationController
     def new
     end
 
-    def create_from_facebook
-        @user = User.find_by(email: auth.info.email)
-        if @user != nil
-          session[:user_id] = @user.id
-          redirect_to user_path(session[:user_id])
-        else
-          @random_pass = SecureRandom.hex
-          @user = User.new(email: auth.info.email, username: auth.info.email)
-          session[:user_id] = user.id
-          redirect_to user_path(session[:user_id])
-        end
-    end
+    # def create_from_facebook
+    #     user = User.find_by(email: auth.info.email)
+    #     if user = nil
+    #         user = User.create(:email => auth["info"]["email"]) do |u|
+    #             u.name = auth["info"]["name"]
+    #             u.password = SecureRandom.hex
+    #         end
+    #       session[:user_id] = user.id
+    #       redirect_to user_path(user)
+    #     else
+    #       session[:user_id] = user.id
+    #       redirect_to user_path(user)
+    #     end
+    # end
 
     def create
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password]) 
-            session[:user_id] = user.id
-            redirect_to user_path(user)
-        else
-            flash[:error] = "Uh oh! Login info incorrect, please try again."
-            redirect_to login_path
+        if auth
+            @user = User.find_or_create_by(:email => auth["info"]["email"]) do |u|
+               u.name = auth["info"]["name"]
+               u.password = SecureRandom.hex(10)
+            end
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            user = User.find_by(email: params[:email])
+            if user && user.authenticate(params[:password]) 
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            else
+                flash[:error] = "Uh oh! Login info incorrect, please try again."
+                redirect_to login_path
+            end
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static#home"
-  match '/auth/:provider/callback' => 'sessions#create_from_facebook', via: [:get, :post]
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
 
   resources :users
   resources :horoscopes
